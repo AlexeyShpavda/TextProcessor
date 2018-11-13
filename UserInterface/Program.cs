692ecc600a8b4075a470703a542bc50bb2bc38e7
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-11-13 22:25:10 +0300

    Add Text Files to Solution to Check Parser Operation

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace UserInterface
+﻿using System;
+using System.Configuration;
+using System.IO;
+using SyntacticalAnalyzer;
+
+namespace UserInterface
 {
     internal class Program
     {
         private static void Main()
         {
+            var readFilePath = ConfigurationManager.AppSettings["readFilePath"];
+
+            var streamReader = new StreamReader(readFilePath);
+            var textParser = new TextParser();
+            var text = textParser.Parse(streamReader);
+
+            Console.ReadKey();
         }
     }
 }
