3dd5323c3fda5c20cd1d6d0cd53a65ce7694a9bf
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-11-14 05:07:02 +0300

    Improve Text Syntactical Analyzer

--- SyntacticalAnalyzer/TextParser.cs
+++ SyntacticalAnalyzer/TextParser.cs
@@ -12,10 +12,10 @@ namespace SyntacticalAnalyzer
 {
     public class TextParser : ITextParser
     {
+        private const string Pattern = @"\b(\w+)((\p{P}{0,3})\s?)";
+
         public IText Parse(StreamReader streamReader)
         {
-            const string pattern = @"\b(\w+)((\p{P}{0,3})\s?)";
-
             var text = new Text();
             var sentence = new Sentence();
 
@@ -23,11 +23,9 @@ namespace SyntacticalAnalyzer
             while ((fileLine = streamReader.ReadLine()) != null)
             {
                 fileLine = string.Concat(fileLine, " ");
-                foreach (Match match in Regex.Matches(fileLine, pattern))
+                foreach (Match match in Regex.Matches(fileLine, Pattern))
                 {
-                    sentence.Add(new Word(match.Groups[1].ToString()));
-
-                    sentence.Add(new Separator(match.Groups[2].ToString()));
+                    AddWordWithSeparatorToSentence(sentence, match.Groups[1].ToString(), match.Groups[2].ToString());
 
                     if (!Separator.IsEndPunctuationSeparator(match.Groups[2].ToString())) continue;
 
@@ -58,21 +56,24 @@ namespace SyntacticalAnalyzer
 
         public ISentence Parse(string inputLine)
         {
-            const string pattern = @"\b(\w+)((\p{P}{0,3})\s?)";
-
             var sentence = new Sentence();
 
             var line = inputLine;
 
             line = string.Concat(line, " ");
-            foreach (Match match in Regex.Matches(line, pattern))
+            foreach (Match match in Regex.Matches(line, Pattern))
             {
-                sentence.Add(new Word(match.Groups[1].ToString()));
-
-                sentence.Add(new Separator(match.Groups[2].ToString()));
+                AddWordWithSeparatorToSentence(sentence, match.Groups[1].ToString(), match.Groups[2].ToString());
             }
 
             return sentence;
+        }
+
+        private static void AddWordWithSeparatorToSentence(ISentence sentence, string word, string separator)
+        {
+            sentence.Add(new Word(word));
+
+            sentence.Add(new Separator(separator));
         }
     }
 }
